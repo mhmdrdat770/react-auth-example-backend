7053c13b2e8d3d0879b4e1f895fc705754c19eb9
mhmdrdat <Mohammad Reza>
2024-03-26 12:11:44 +0330

    coding register controller

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function register(){
+    public function register(RegisterRequest $request){
+        $inputs = $request->only(['name','email', 'password']);
+
+        $inputs['password'] = Hash::make($inputs['password']);
+
+        $user = User::create($inputs);
+
+        auth()->login($user);
+
+
+        $token = $user->createToken('API_TOKEN')->plainTextToken;
+
+        $cookie = cookie('auth_token', $token,1440);
+
+        $response = [
+            'data' => [],
+            'message' => 'با موفقیت ثبت نام کردید',
+            'success' => true,
+        ];
+
+        return response()->json($response,200)->withCookie($cookie);
+
 
     }
 }
