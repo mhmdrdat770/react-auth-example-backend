619866847f19bc7a007bce15025685886e7a8ae9
mhmdrdat <Mohammad Reza>
2024-03-26 11:53:05 +0330

    create rotues

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function(){
+    Route::post('/login',[LoginController::class,'login']);
+    Route::post('/register',[RegisterController::class,'register']);
+});
