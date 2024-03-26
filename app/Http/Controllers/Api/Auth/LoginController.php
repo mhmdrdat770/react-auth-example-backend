c98442db6669e8c0467c4ede4c23c1a2571803c4
mhmdrdat <Mohammad Reza>
2024-03-26 12:07:55 +0330

    coding lagin controller

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -3,11 +3,36 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\Auth\LoginRequest;
 
 class LoginController extends Controller
 {
-    public function login(){
+    public function login(LoginRequest $request)
+    {
+        $inputs = $request->only(['email', 'password']);
 
+        if (!auth()->attempt(['email' => $inputs['email'], 'password' => $inputs['password']])) {
+            $response = [
+                'data' => [],
+                'message' => 'اطلاعات کاربری درست نیست',
+                'success' => false,
+            ];
+
+            return response()->json($response, 401);
+        }
+
+        $user = auth()->user();
+
+        $token = $user->createToken('API_TOKEN')->plainTextToken;
+
+        $cookie = cookie('auth_token', $token,1440);
+
+        $response = [
+            'data' => [],
+            'message' => 'با موفقیت وارد شدید',
+            'success' => true,
+        ];
+
+        return response()->json($response,200)->withCookie($cookie);
     }
 }
