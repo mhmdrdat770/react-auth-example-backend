340e24cc24b421f6fee40b11867a26198a19b95f
mhmdrdat <Mohammad Reza>
2024-03-26 11:55:20 +0330

    add user route

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,14 @@
 
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 
 Route::prefix('auth')->group(function(){
+    Route::get('/user',[UserController::class, 'user'])->middleware('auth:sanctum');
     Route::post('/login',[LoginController::class,'login']);
     Route::post('/register',[RegisterController::class,'register']);
 });
