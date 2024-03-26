7bfdb4f27bef0ddabe73e932cd22d7c3b58d32e8
mhmdrdat <Mohammad Reza>
2024-03-26 13:45:47 +0330

    add logout route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\Auth\UserController;
 use Illuminate\Http\Request;
@@ -12,4 +13,5 @@ Route::prefix('auth')->group(function(){
     Route::get('/user',[UserController::class, 'user'])->middleware('auth:sanctum');
     Route::post('/login',[LoginController::class,'login']);
     Route::post('/register',[RegisterController::class,'register']);
+    Route::get('/logout',[LogoutController::class, 'logout'])->middleware('auth:sanctum');
 });
